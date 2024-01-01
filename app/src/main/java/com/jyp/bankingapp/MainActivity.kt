fec27eca4d98546f3523d051c99ff453391a369a
JJongmen <pjy90123@gmail.com>
2024-01-02 01:27:50 +0900

    feat: system ui 색깔 통일

--- app/src/main/java/com/jyp/bankingapp/MainActivity.kt
+++ app/src/main/java/com/jyp/bankingapp/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jyp.bankingapp.ui.theme.BankingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +19,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             BankingAppTheme {
                 // A surface container using the 'background' color from the theme
+
+                SetBarColor(color = MaterialTheme.colorScheme.background)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankingAppTheme {
-        Greeting("Android")
+    @Composable
+    private fun SetBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setSystemBarsColor(
+                color = color
+            )
+        }
     }
 }
