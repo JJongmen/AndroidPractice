0e891ed6e0f16d082b826f1017ad6b9f59a293ed
JJongmen <pjy90123@gmail.com>
2024-01-02 01:48:25 +0900

    feat: 하단 네비게이션바 구현

--- app/src/main/java/com/jyp/bankingapp/MainActivity.kt
+++ app/src/main/java/com/jyp/bankingapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.jyp.bankingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jyp.bankingapp.ui.theme.BankingAppTheme
 
@@ -25,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    HomeScreen()
                 }
             }
         }
@@ -39,5 +46,31 @@ class MainActivity : ComponentActivity() {
                 color = color
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+
+        }
+
+//        WalletSection()
+//        CardsSection()
+        Spacer(modifier = Modifier.height(16.dp))
+//        FinanceSection()
+//        CurrenciesSection()
+
     }
 }
